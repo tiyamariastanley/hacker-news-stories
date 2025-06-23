8a781145b2bc2359cd63064e76598b31b670ff87
Tiya Maria Stanley <tiyamaria96@gmail.com>
2025-06-23 22:46:07 +0200

    Bug fix

--- src/util/fetcher.ts
+++ src/util/fetcher.ts
@@ -4,7 +4,7 @@ export const fetcher = async (url: string) => {
   if (!res.ok) {
     const error = new Error(
       "An error occurred while fetching the data."
-    ) as Error & { info?: any; status?: number };
+    ) as Error & { info?: string; status?: number };
     error.info = await res.json();
     error.status = res.status;
     throw error;
